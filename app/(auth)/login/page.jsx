c03d8b2ff1eace4e7619fa437792b57c80bc886e
Arya Priema <99aryaprima@gmail.com>
2023-08-03 13:15:28 +0700

    Login Page: Implement automatic redirection to protected route when session token exists

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,6 +1,13 @@
 import { Login } from "./(components)/index "
 
-const LoginPage = () => {
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/utils/auth"
+import { redirect } from "next/navigation";
+
+const LoginPage = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (session) redirect('/dashboard');
   return (
     <div>
         <Login />
