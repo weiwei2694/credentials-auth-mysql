4956807984a7028bcddc7cf1267c18c5558ccf2a
Arya Priema <99aryaprima@gmail.com>
2023-08-03 13:16:28 +0700

    Navbar Component: styles

--- app/(protected)/(components)/Navbar.jsx
+++ app/(protected)/(components)/Navbar.jsx
@@ -4,9 +4,9 @@ import { signOut } from "next-auth/react";
 const Navbar = () => {
   return (
     <div className="py-3 border-b border-b-gray-200">
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container flex justify-between items-center">
         {/* Image */}
-        <div className="w-[60px] h-[60px] rounded-full bg-gray-100" />
+        <div className="w-[50px] h-[50px] rounded-full bg-gray-100" />
 
         {/* SignOut */}
         <button
