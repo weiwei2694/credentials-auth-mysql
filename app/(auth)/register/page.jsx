ecee82189677f8fad9eb4735a5e595352096eafd
Arya Priema <99aryaprima@gmail.com>
2023-08-03 13:14:29 +0700

    Register Page: Implement automatic redirection to protected route when session token exists

--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -1,6 +1,14 @@
 import { Register } from "./(components)"
 
-const RegisterPage = () => {
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/utils/auth"
+import { redirect } from "next/navigation";
+
+const RegisterPage = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (session) redirect('/dashboard');
+  
   return (
     <div>
         <Register />
