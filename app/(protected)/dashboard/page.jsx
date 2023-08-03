0b06879a1647982a03848d453d6bf875ac527672
Arya Priema <99aryaprima@gmail.com>
2023-08-03 12:55:39 +0700

    Protect route from unauthorized access with session

--- app/(protected)/dashboard/page.jsx
+++ app/(protected)/dashboard/page.jsx
@@ -1,4 +1,13 @@
-const DashboardPage = () => {
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/utils/auth"
+
+import { redirect } from "next/navigation"
+
+const DashboardPage = async () => {
+    const session = await getServerSession(authOptions)
+
+    if (!session) redirect('/login');
+    
   return (
     <div>DashboardPage</div>
   )
