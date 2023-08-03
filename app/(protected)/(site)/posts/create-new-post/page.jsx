f519d124b4cc1dcdc79af1edd159480e5cc6064a
Arya Priema <99aryaprima@gmail.com>
2023-08-03 16:23:31 +0700

    Create New Post Page: Protect route from unauthorized access with session

--- app/(protected)/(site)/posts/create-new-post/page.jsx
+++ app/(protected)/(site)/posts/create-new-post/page.jsx
@@ -1,6 +1,14 @@
 import { CreateNewPost } from "./(components)"
 
-const CreateNewPostPage = () => {
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/utils/auth"
+import { redirect } from "next/navigation"
+
+const CreateNewPostPage = async () => {
+    const session = await getServerSession(authOptions)
+
+    if (!session) redirect('/login');
+
   return (
     <div className="container max-w-[450px] mt-6">
         <CreateNewPost />
