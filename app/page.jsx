e25170dd92c2b89f92a3d4ef3d328cfa020f9d7f
Arya Priema <99aryaprima@gmail.com>
2023-08-03 12:38:31 +0700

    Home Page: styles, redirect('/login' & '/register')

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,18 @@
+import Link from "next/link"
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <main className="p-16">
+    <h1 className="text-3xl font-extrabold mb-4">NextAuth.js</h1>
+    <ul className="flex flex-col gap-2 list-disc">
+      <li>
+        <Link href="/register" className="font-semibold text-black underline">Register Page</Link>
+      </li>
+      <li>
+        <Link href="/login" className="font-semibold text-black underline">Login Page</Link>
+      </li>
+    </ul>
+  </main>
   )
 }
 
