d4222ae90519b05b83ec7c5a08a3e446b491443a
Arya Priema <99aryaprima@gmail.com>
2023-08-03 12:36:23 +0700

    Authentication

---
+++ utils/auth.js
@@ -0,0 +1,64 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import db from "@/lib/db";
+
+import { compare } from "bcrypt";
+
+export const authOptions = {
+  adapter: PrismaAdapter(db),
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        name: { label: "Name", type: "text", placeholder: "Wei Wei" },
+        email: { label: "Email", type: "email", placeholder: "weiwei@gmail.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials;
+
+        if (!email || !password) return null;
+
+        const user = await db.user.findUnique({
+          where: {
+            email,
+          },
+        });
+
+        if (!user) return null;
+
+        const passwordMatch = compare(password, user.hashedPassword);
+
+        if (!passwordMatch) return null;
+
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        return {
+          ...token,
+          userId: user.id,
+        };
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          userId: token.userId,
+        },
+      };
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: process.env.NODE_ENV === "development",
+};
