ef335178c910ef164334363cd7141886271a0110
Arya Priema <99aryaprima@gmail.com>
2023-08-03 17:03:48 +0700

    Create New Post Page: Move 'Create New Post' button to justify-start in PostsNavigation component

--- app/(protected)/(site)/posts/(components)/PostsNavigation.jsx
+++ app/(protected)/(site)/posts/(components)/PostsNavigation.jsx
@@ -6,7 +6,7 @@ const PostsNavigation = () => {
 
   return (
     <div className="flex">
-        <div className="flex-1 flex justify-end">
+        <div className="flex-1 flex justify-start">
             <button type="button" className="py-2 px-6 bg-white hover:bg-gray-50 active:bg-gray-100 transition text-black border border-gray-200 font-medium rounded" onClick={() => router.push('/posts/create-new-post')}>Create New Post</button>
         </div>
     </div>
