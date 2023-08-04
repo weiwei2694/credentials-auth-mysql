8bf7d507481de18df288ad7bc95b3da7a6e54af8
Arya Priema <99aryaprima@gmail.com>
2023-08-04 16:11:47 +0700

    Reset Password Page: Protect route from unauthorized access with session

--- app/(forgot-password)/accounts/reset-password/page.jsx
+++ app/(forgot-password)/accounts/reset-password/page.jsx
@@ -1,6 +1,13 @@
 import { ResetPassword } from "./(components)"
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/utils/auth";
+import { redirect } from "next/navigation";
 
-const ResetPasswordPage = () => {
+const ResetPasswordPage = async () => {
+  const session = await getServerSession(authOptions)
+
+  if (session) redirect('/dashboard');
+  
   return (
     <div>
         <ResetPassword />
