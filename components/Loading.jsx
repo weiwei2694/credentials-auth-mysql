07d3ac8e6a3de901d003b8c3d57e205c54cc0337
Arya Priema <99aryaprima@gmail.com>
2023-08-04 16:18:54 +0700

    Create Loading Component

---
+++ components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+      <div className="min-h-screen w-full bg-white grid place-items-center">
+          <p>Loading...</p>
+      </div>
+    )
+  }
+  
+  export default Loading
