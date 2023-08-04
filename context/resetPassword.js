1a9a306e82742c31081d6542599fa0f8e123a4ad
Arya Priema <99aryaprima@gmail.com>
2023-08-05 06:22:13 +0700

    resetPassword: delete validOtp & setValidOtp

--- context/resetPassword.js
+++ context/resetPassword.js
@@ -1,9 +1,7 @@
 import { create } from 'zustand'
 
 const useOtp = create((set) => ({
-  validOtp: false,
   currentPage: '',
-  setValidOtp: () => set(state => ({ ...state, validOtp: !state.validOtp })),
   setCurrentPage: ({ newCurrentPage }) => set(state => ({ ...state, currentPage: newCurrentPage })),
 }))
 
