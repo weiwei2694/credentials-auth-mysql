b956c31481a60a79942ff05b6f23b96858488dcf
Arya Priema <99aryaprima@gmail.com>
2023-08-04 18:43:07 +0700

    Email End Point: added additional validation, for features that some accounts can't reach

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { transporter, mailOptions } from "@/config/nodemailer";
+import db from "@/lib/db";
 
 export async function POST(req){
     const { email, otp } = await req.json();
@@ -7,6 +8,19 @@ export async function POST(req){
     if (!email || !otp) return new NextResponse('Missing email or otp', { status: 400 })
 
     try {
+        const user = await db.user.findUnique({
+            where: {
+                email
+            },
+            include: {
+                accounts: true
+            }
+        })
+
+        const accountsExist = user?.accounts;
+
+        if (accountsExist.length !== 0) return new NextResponse('This email has been registered via OAuth and cannot use the forgot password feature.', { status: 403 })
+
         await transporter.sendMail({
             ...mailOptions,
             to: email,
