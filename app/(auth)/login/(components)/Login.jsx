06879c46caaa81c639fa42dda5fd3bcd435403c0
Arya Priema <99aryaprima@gmail.com>
2023-08-05 00:39:04 +0700

    Login Page: styles

--- app/(auth)/login/(components)/Login.jsx
+++ app/(auth)/login/(components)/Login.jsx
@@ -67,57 +67,59 @@ const Login = ({ callbackError }) => {
           </p>
         </div>
 
-        <form className="flex flex-col gap-20" onSubmit={loginUser}>
-          <div className="flex flex-col gap-10">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm text-gray-900"
-              >
-                Email
-              </label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                value={data.email}
-                autoComplete="email"
-                onChange={(e) => setData({ ...data, email: e.target.value })}
-                required
-                className="block w-full border-b outline-none border-b-gray-200 focus:border-b-2 focus:border-b-gray-400 pt-1.5 text-black placeholder:text-gray-400 sm:text-sm sm:leading-6"
-              />
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
+        <form className="flex flex-col gap-5" onSubmit={loginUser}>
+          <div className="flex flex-col gap-20">
+            <div className="flex flex-col gap-10">
+              <div>
                 <label
-                  htmlFor="password"
-                  className="block text-sm leading-6 text-gray-900"
+                  htmlFor="email"
+                  className="block text-sm text-gray-900"
                 >
-                  Password
+                  Email
                 </label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={data.email}
+                  autoComplete="email"
+                  onChange={(e) => setData({ ...data, email: e.target.value })}
+                  required
+                  className="block w-full border-b outline-none border-b-gray-200 focus:border-b-2 focus:border-b-gray-400 pt-1.5 text-black placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                />
               </div>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                value={data.password}
-                onChange={(e) => setData({ ...data, password: e.target.value })}
-                required
-                className="block w-full border-b outline-none border-b-gray-200 focus:border-b-2 focus:border-b-gray-400 pt-1.5 text-black placeholder:text-gray-400 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
 
-          <div>
-            <button
-              type="submit"
-              disabled={mutation}
-              className="flex w-full justify-center rounded-md bg-indigo-600 disabled:bg-indigo-800 px-3 py-3 font-medium leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              {mutation ? "Login..." : "Login"}
-            </button>
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm leading-6 text-gray-900"
+                  >
+                    Password
+                  </label>
+                </div>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  value={data.password}
+                  onChange={(e) => setData({ ...data, password: e.target.value })}
+                  required
+                  className="block w-full border-b outline-none border-b-gray-200 focus:border-b-2 focus:border-b-gray-400 pt-1.5 text-black placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                disabled={mutation}
+                className="flex w-full justify-center rounded-md bg-indigo-600 disabled:bg-indigo-800 px-3 py-3 font-medium leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                {mutation ? "Login..." : "Login"}
+              </button>
+            </div>
           </div>
 
           <div className="text-center">
@@ -129,7 +131,7 @@ const Login = ({ callbackError }) => {
       </div>
 
       {/* Divider */}
-      <div className="my-16 w-full">
+      <div className="my-10 w-full">
         <div className="relative">
           <div className="flex justify-center items-center absolute top-0 left-0 right-0 bottom-0">
             <h2 className="w-fit font-medium text-gray-500 tracking-wide p-2 bg-white">Or</h2>
